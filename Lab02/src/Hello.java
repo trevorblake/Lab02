ca6e41d11b51313ddd8d67f6487eaf6e55f7d472
VictoriaLarson <victorialarson@csus.edu>
2021-09-18 18:15:53 -0700

    Added by VictoriaL

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("302253913  - This line was added by Trevor Blake.");
 		System.out.println("302728309  - This line was added by Justin Roome.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302154827  - This line was added by Victoria Larson.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
