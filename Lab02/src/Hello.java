0050651b98d2c726e47a63eb09addaa6c82f9d0c
harte <harte@192.168.0.20>
2021-09-20 16:43:21 -0700

    added my name

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("302253913  - This line was added by Trevor Blake.");
 		System.out.println("302728309  - This line was added by Justin Roome.");
 		System.out.println("302154827  - This line was added by Victoria Larson.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302374111  - This line was added by Hartej singh.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
