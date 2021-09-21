366b11174f138f4d344c608e8dd09984cea56cc8
amanj <amanj@192.168.0.34>
2021-09-20 20:53:37 -0700

    Hello Java Amanjot Singh

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("302374111  - This line was added by Hartej singh.");
 		System.out.println("215981832 - This line was added by Oksana Daniliuk.");
 		System.out.println("219090899 - This line was added by Fernando Ramos.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219462868 - This line was added by Amanjot Singh.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
